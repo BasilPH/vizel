2c9ba296a8abbb0bbc645961ee6aa61fa44d1d66
Basil Philipp <basil@interdimensional-television.com>
2020-12-29 21:29:51 -0500

    ♻️  Format with black

--- vizel/cli.py
+++ vizel/cli.py
@@ -12,10 +12,10 @@ from graphviz import Digraph
 @click.group()
 def main():
     """
-    See the stats and connections of your Zettelkasten.
-    \f
+     See the stats and connections of your Zettelkasten.
+     \f
 
-   :return: None
+    :return: None
     """
     pass
 
@@ -32,12 +32,12 @@ def _extract_valid_references(reference_regexp, zettel_path, zettel_filenames):
     :return: Filenames of Zettel that are referenced.
     """
     references = []
-    with open(zettel_path, 'r') as zettel_file:
+    with open(zettel_path, "r") as zettel_file:
         zettel_text = zettel_file.read()
 
         # Internally we only want to deal with unicode
         if six.PY2:
-            zettel_text = unicode(zettel_text, errors='strict')
+            zettel_text = unicode(zettel_text, errors="strict")
 
     reference_texts = re.findall(reference_regexp, zettel_text)
     for reference_text in reference_texts:
@@ -50,15 +50,20 @@ def _extract_valid_references(reference_regexp, zettel_path, zettel_filenames):
             references += matching_zettel_filenames
         elif len(matching_zettel_filenames) > 1:
             click.echo(
-                'Skipping non-unique reference "{}" in {}. Candidates: {}'.format(reference_text,
-                                                                                  os.path.basename(zettel_path),
-                                                                                  ', '.join(
-                                                                                      matching_zettel_filenames)),
-                err=True)
+                'Skipping non-unique reference "{}" in {}. Candidates: {}'.format(
+                    reference_text,
+                    os.path.basename(zettel_path),
+                    ", ".join(matching_zettel_filenames),
+                ),
+                err=True,
+            )
         else:
             click.echo(
-                'No matching Zettel for reference "{}" in {}'.format(reference_text, os.path.basename(zettel_path)),
-                err=True)
+                'No matching Zettel for reference "{}" in {}'.format(
+                    reference_text, os.path.basename(zettel_path)
+                ),
+                err=True,
+            )
     return references
 
 
@@ -72,15 +77,23 @@ def _load_references(zettel_path, zettel_directory_path):
     """
     references = []
     zettel_filenames = sorted(
-        [os.path.basename(f) for f in glob.glob(os.path.join(zettel_directory_path, '*[.md|.txt]'))])
+        [
+            os.path.basename(f)
+            for f in glob.glob(os.path.join(zettel_directory_path, "*[.md|.txt]"))
+        ]
+    )
 
     # Extract references for the [[ID]] link format
     # Look for [[, and then match anything that isn't ]]. End with ]].
-    references += _extract_valid_references('\[\[([^\]\]]+)\]\]', zettel_path, zettel_filenames)
+    references += _extract_valid_references(
+        "\[\[([^\]\]]+)\]\]", zettel_path, zettel_filenames
+    )
 
     # Extract references for the markdown link format
     # Look for [, and then match anything that isn't ]. Then look for ( and match anything that isn't ). End with ).
-    references += _extract_valid_references('\[[^\]]+\]\(([^\)]+)\)', zettel_path, zettel_filenames)
+    references += _extract_valid_references(
+        "\[[^\]]+\]\(([^\)]+)\)", zettel_path, zettel_filenames
+    )
 
     return references
 
@@ -93,15 +106,15 @@ def _get_short_description(zettel_filename):
     """
 
     # Create a short, 50 character, description
-    replace_with_space = ['_', '-']
-    remove = ['.md', '.txt']
+    replace_with_space = ["_", "-"]
+    remove = [".md", ".txt"]
     short_des = zettel_filename
 
     for replace_char in replace_with_space:
-        short_des = short_des.replace(replace_char, ' ')
+        short_des = short_des.replace(replace_char, " ")
 
     for remove_char in remove:
-        short_des = short_des.replace(remove_char, '')
+        short_des = short_des.replace(remove_char, "")
 
     return short_des
 
@@ -116,7 +129,9 @@ def _get_digraph(zettel_directory_path):
 
     digraph = nx.DiGraph()
 
-    for zettel_path in sorted(glob.glob(os.path.join(zettel_directory_path, '*[.md|.txt]'))):
+    for zettel_path in sorted(
+        glob.glob(os.path.join(zettel_directory_path, "*[.md|.txt]"))
+    ):
 
         zettel_filename = os.path.basename(zettel_path)
         short_des = _get_short_description(zettel_filename)
@@ -124,11 +139,13 @@ def _get_digraph(zettel_directory_path):
         digraph.add_node(zettel_filename, short_description=short_des, path=zettel_path)
 
         try:
-            for reference_zettel_filename in _load_references(zettel_path, zettel_directory_path):
+            for reference_zettel_filename in _load_references(
+                zettel_path, zettel_directory_path
+            ):
                 if zettel_filename != reference_zettel_filename:
                     digraph.add_edge(zettel_filename, reference_zettel_filename)
         except UnicodeDecodeError as e:
-            click.echo('Skipping {}: {}'.format(zettel_filename, e), err=True)
+            click.echo("Skipping {}: {}".format(zettel_filename, e), err=True)
     return digraph
 
 
@@ -137,16 +154,19 @@ def _get_zero_degree_nodes(digraph):
     Get all the nodes that have degree zero
 
     :param digraph: DiGraph object representing the Zettel graph.
-    :return: List of nodes from `digraph` where degree is 0. 
+    :return: List of nodes from `digraph` where degree is 0.
     """
 
     return [node for node, degree in digraph.degree() if degree == 0]
 
 
-@main.command(short_help='PDF of Zettel graph')
-@click.argument('directory', type=click.Path(exists=True, dir_okay=True))
-@click.option('--pdf-name', default='vizel_graph.pdf',
-              help='Name of the PDF file the graph is written into. Default: vizel_graph.pdf')
+@main.command(short_help="PDF of Zettel graph")
+@click.argument("directory", type=click.Path(exists=True, dir_okay=True))
+@click.option(
+    "--pdf-name",
+    default="vizel_graph.pdf",
+    help="Name of the PDF file the graph is written into. Default: vizel_graph.pdf",
+)
 def graph_pdf(directory, pdf_name):
     """
     Generates a PDF of the graph spanned by Zettel in DIRECTORY.
@@ -159,22 +179,22 @@ def graph_pdf(directory, pdf_name):
 
     digraph = _get_digraph(directory)
 
-    dot = Digraph(comment='Zettelkasten Graph')
+    dot = Digraph(comment="Zettelkasten Graph")
 
     for (node, data) in digraph.nodes(data=True):
-        dot.node(node, data['short_description'])
+        dot.node(node, data["short_description"])
 
     for u, v in digraph.edges:
         dot.edge(u, v)
 
     # Remove the last `.pdf` ending if present
-    if pdf_name.endswith('.pdf'):
-        pdf_name = pdf_name.rpartition('.pdf')[0]
+    if pdf_name.endswith(".pdf"):
+        pdf_name = pdf_name.rpartition(".pdf")[0]
     dot.render(pdf_name, cleanup=True)
 
 
-@main.command(short_help='Stats of Zettel graph')
-@click.argument('directory', type=click.Path(exists=True, dir_okay=True))
+@main.command(short_help="Stats of Zettel graph")
+@click.argument("directory", type=click.Path(exists=True, dir_okay=True))
 def stats(directory):
     """
     Prints the stats of the graph spanned by Zettel in DIRECTORY.
@@ -193,17 +213,21 @@ def stats(directory):
 
     digraph = _get_digraph(directory)
 
-    click.echo('{} Zettel'.format(digraph.number_of_nodes()))
-    click.echo('{} references between Zettel'.format(digraph.number_of_edges()))
+    click.echo("{} Zettel".format(digraph.number_of_nodes()))
+    click.echo("{} references between Zettel".format(digraph.number_of_edges()))
 
     n_nodes_no_edges = len(_get_zero_degree_nodes(digraph))
-    click.echo('{} Zettel with no references'.format(n_nodes_no_edges))
+    click.echo("{} Zettel with no references".format(n_nodes_no_edges))
 
-    click.echo('{} connected components'.format(nx.number_connected_components(digraph.to_undirected())))
+    click.echo(
+        "{} connected components".format(
+            nx.number_connected_components(digraph.to_undirected())
+        )
+    )
 
 
-@main.command(short_help='Zettel without references')
-@click.argument('directory', type=click.Path(exists=True, dir_okay=True))
+@main.command(short_help="Zettel without references")
+@click.argument("directory", type=click.Path(exists=True, dir_okay=True))
 def unconnected(directory):
     """
     Prints all of the Zettel in DIRECTORY that have no in- or outgoing references.
@@ -219,11 +243,11 @@ def unconnected(directory):
     zero_degree_nodes = _get_zero_degree_nodes(digraph)
 
     for node in sorted(zero_degree_nodes):
-        click.echo('{}'.format(node))
+        click.echo("{}".format(node))
 
 
-@main.command(short_help='Connected components')
-@click.argument('directory', type=click.Path(exists=True, dir_okay=True))
+@main.command(short_help="Connected components")
+@click.argument("directory", type=click.Path(exists=True, dir_okay=True))
 def components(directory):
     """
     Lists the connected components and their Zettel in DIRECTORY.
@@ -246,7 +270,7 @@ def components(directory):
     conn_components = sorted(conn_components, key=len, reverse=True)
 
     for i, component in enumerate(conn_components, start=1):
-        click.echo('# Component {}'.format(i))
+        click.echo("# Component {}".format(i))
         for zettel in component:
             click.echo(zettel)
 
